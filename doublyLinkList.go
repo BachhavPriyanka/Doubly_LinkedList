2076912ac8750a1d79c6ccc56c359ac710d6588d
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2023-02-26 16:42:28 +0530

    Added: Method for adding node at first

--- doublyLinkList.go
+++ doublyLinkList.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Value    int
 	Next     *Node
@@ -9,4 +11,33 @@ type DoublyLinkedList struct {
 	Head *Node
 	Tail *Node
 	Size int
+}
+
+func (ll *DoublyLinkedList) AddAtFirst(data int) {
+	newNode := &Node{Value: data}
+
+	if ll.Head == nil {
+		ll.Head = newNode
+		ll.Tail = newNode
+	} else {
+		ll.Head.Previous = newNode
+		newNode.Next = ll.Head
+		ll.Head = newNode
+	}
+}
+
+func (ll *DoublyLinkedList) Display() {
+	current := ll.Head
+	for current != nil {
+		fmt.Print(current.Value, " ")
+		current = current.Next
+	}
+	fmt.Println("")
+}
+func main() {
+	List := DoublyLinkedList{}
+
+	List.AddAtFirst(1)
+	List.AddAtFirst(22)
+	List.Display()
 }
